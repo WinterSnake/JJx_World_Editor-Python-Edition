05fd386aeff6c45dce96c8722e3621791240deee
Winter_Snake <blizzardsnake98@gmail.com>
2019-03-07 23:01:01 -0500

    Backend refinement.

--- src/world/world_components.py
+++ src/world/world_components.py
@@ -6,7 +6,7 @@
 ##---------------------------------------------##
 
 ## Imports
-from typing import Union
+from typing import Union, Tuple, List
 import json, os.path as ospath, struct
 
 ## Constants
@@ -84,13 +84,17 @@ class Item(object):
 	pass
 	# -Constructor
 	def __init__(self):
-		pass
+		self.id = ""
+		self.count = 0
+		self.durability = 0
+		self.render_id = 0
+		self.modifier = ""
 
 	# -Dunder Methods
 
 	# -Class Methods
 	@classmethod
-	def Create_From_ByteString(cls, bytestring: bytes, slot: int):
+	def Create_From_ByteString(cls, bytestring: bytes):
 		''''''
 		pass
 
@@ -111,7 +115,8 @@ class Entity(object):
 	pass
 	# -Constructor
 	def __init__(self):
-		pass
+		self.id = ""
+		self.position = tuple()
 
 	# -Dunder Methods
 
@@ -134,7 +139,9 @@ class Chest(object):
 	pass
 	# -Constructor
 	def __init__(self):
-		pass
+		self.slots = 0
+		self.position = tuple()
+		self.item_container = list()
 
 	# -Dunder Methods
 
@@ -152,6 +159,140 @@ class Chest(object):
 		pass
 
 	# -Class Variables
+
+class World_Properties(object):
+	pass
+	# -Constructor
+	def __init__(self, name: str, size: str, planet: str, gamemode: str, spawn: List[int], player: List[int], border: List[int], skybox_offset: int, last_played: int):
+		self.world_name = name
+		self.world_size = size
+		self.planet = planet
+		self.gamemode = gamemode
+		self.spawn_position = spawn
+		self.player_position = player
+		self.border_array = border
+		self.skybox_offset = skybox_offset
+		self.last_played = last_played
+
+	# -Dunder Methods
+	def __str__(self):
+		return (f"{self.world_name} Properties:"
+				f"\n\tSize: {self.world_size}"
+				f"\n\tPlanet: {self.planet}"
+				f"\n\tGamemode: {self.gamemode}"
+				f"\n\tSpawn Position: {self.spawn_position}"
+				f"\n\tPlayer Position: {self.player_position}"
+				f"\n\tWorld Border: {self.border_array}"
+				f"\n\tSkybox Offset: {self.skybox_offset}"
+				f"\n\tLast Played: {self.last_played}")
+
+	def __repr__(self):
+		return (f"Properties('{self.world_name}', '{self.world_size}', '{self.planet}', '{self.gamemode}', {self.spawn_position}, {self.player_position}, {self.border_array}, {self.skybox_offset}, {self.last_played})")
+
+	# -Class Methods
+	@classmethod
+	def Create_From_ByteString(cls, bytestring: bytes):
+		'''Returns a world properties object from the given bytestring.'''
+
+		## Parameter Strip
+		world_name = bytestring[8:39].decode('utf-8').strip('\0')
+		player_position = list(struct.unpack('<2h', bytestring[60:64]))
+		spawn_position = list(struct.unpack('<2h', bytestring[64:68]))
+		last_played = struct.unpack('<Q', bytestring[0:8])[0]
+		border_array = World_Properties._Create_Border_Array_From_ByteString(bytestring[208:])
+		skybox_offset = struct.unpack('<B', bytestring[75:76])[0]
+		planet, gamemode, world_size = World_Properties._Compare_Property_Flag(planet_bytestring = bytestring[68:70], gamemode_bytestring = bytestring[73:74], size_bytestring = bytestring[74:75])
+
+		return cls(world_name, world_size, planet, gamemode, spawn_position, player_position, border_array, skybox_offset, last_played)
+
+	# -Static Methods
+	@staticmethod
+	def _Compare_Property_Flag(planet_bytestring: bytes = None, gamemode_bytestring: bytes = None, size_bytestring: bytes = None) -> Tuple[bytes, bytes, bytes]:
+		'''Recieves bytestring for planet, gamemode, and world size. Returns the string variant of the given bytestrings.'''
+		# -Planet Parameter
+		if planet_bytestring == b'\x01\x00':
+			planet = "Terra"
+		elif planet_bytestring == b'\x02\x00':
+			planet = "Seth"
+		elif planet_bytestring == b'\x04\x00':
+			planet = "Alba"
+		elif planet_bytestring == b'\x08\x00':
+			planet = "Xeno"
+		elif planet_bytestring == b'\x10\x00':
+			planet = "Magmar"
+		elif planet_bytestring == b'\x20\x00':
+			planet = "Cryo"
+		elif planet_bytestring == b'\x40\x00':
+			planet = "Yuca"
+		elif planet_bytestring == b'\x80\x00':
+			planet = "Lilith"
+		elif planet_bytestring == b'\x00\x01':
+			planet = "Thetis"
+		elif planet_bytestring == b'\x00\x02':
+			planet = "Mykon"
+		elif planet_bytestring == b'\x00\x04':
+			planet = "Umbra"
+		else:
+			planet = None
+
+		# -Gamemode Parameter
+		if gamemode_bytestring == b'\x00':
+			gamemode = "Survival"
+		elif gamemode_bytestring == b'\x01':
+			gamemode = "Creative"
+		elif gamemode_bytestring == b'\x02':
+			gamemode = "Flat"
+		elif gamemode_bytestring == b'\x03':
+			gamemode = "Adventure"
+		else:
+			gamemode = None
+
+		# -Size Parameter
+		if size_bytestring == b'\x00':
+			size = "Tiny"
+		elif size_bytestring == b'\x01':
+			size = "Small"
+		elif size_bytestring == b'\x02':
+			size = "Normal"
+		elif size_bytestring == b'\x03':
+			size = "Large"
+		elif size_bytestring == b'\x04':
+			size = "Huge"
+		else:
+			size = None
+
+		return planet, gamemode, size
+
+	@staticmethod
+	def _Create_Border_Array_From_ByteString(border_bytestring: bytes):
+		'''Returns a list of integers for each size of the border.'''
+		border_array = []
+		for i in range(int(len(border_bytestring) / 32)):
+			if i % 2 == 0:
+				pre_border_array = struct.unpack('<32h', border_bytestring[i : i + 64])
+
+				for border_value in pre_border_array:
+					border_array.append(border_value)
+
+		return border_array
+
+	# -Instance Methods
+	def Translate_To_Dictionary(self) -> dict:
+		''''''
+		return {
+			"world" : {
+				"name" : self.world_name,
+				"size" : self.world_size
+			},
+			"border" : self.border_array
+		}
+
+	def Translate_To_ByteString(self) -> bytes:
+		''''''
+		pass
+
+	# -Class Variables
+
 
 ## Main
 
--- src/world/world_io.py
+++ src/world/world_io.py
@@ -6,7 +6,7 @@
 ##---------------------------------------------##
 
 ## Imports
-from typing import Tuple, BinaryIO, TypeVar
+from typing import Tuple, BinaryIO
 
 import struct, gzip, zlib, io
 
@@ -21,6 +21,20 @@ jjx_adv_header = b'\x4A\x4A\x58\x4D\x02\x00\x02\x00\x00\x00\x00\x00\x01\x00\x00\
 
 ## Functions
 
+##-------- Format Compression/Decompression --------##
+
+def Decompress_Byte_Segment(bytestring: bytes) -> bytes:
+	'''Returns the decompressed bytestring of the given bytestring using GZip encoding - level 6.'''
+	return zlib.decompress(bytestring, 32)
+
+def Compress_Byte_Segment(bytestring: bytes) -> bytes:
+	'''Returns the compressed bytestring of the given bytestring in GZip encoding - level 6.'''
+	compressed_segment = io.BytesIO()
+	with gzip.GzipFile(fileobj = compressed_segment, mode = 'wb', compresslevel = 6, mtime = 0.0) as compression_stream:
+		compression_stream.write(bytestring)
+
+	return compressed_segment.getvalue()
+
 ##-------- Retrieve World Data --------##
 
 def Import_World(file_path: str) -> Tuple[dict, bytes]:
@@ -33,7 +47,7 @@ def Import_World(file_path: str) -> Tuple[dict, bytes]:
 		if (file_header == jjx_map_header):
 			print("Single-World Import")
 
-			return Decompress_World_File(importing_world, debug_text = True)
+			return Parse_World_File(importing_world, debug_text = True)
 
 		# -Adventure Worlds Import
 		elif (file_header == jjx_adv_header):
@@ -44,43 +58,65 @@ def Import_World(file_path: str) -> Tuple[dict, bytes]:
 			print("Invalid World Format.")
 			return None
 
-def Decompress_World_File(world_file_object: BinaryIO, **kwargs) -> Tuple[dict, bytes]:
+def Parse_World_File(world_file_object: BinaryIO, **kwargs) -> Tuple[dict, bytes]:
 	'''
 	Returns a dictionary of the world properties and a bytestring of the decompressed world.
-	If debug mode is enabled, will return world properties, decompressed world, full header, and full footer.'''
+	If debug mode is enabled, will return world properties, decompressed world, full header (bytes), and discover footer (bytes) and the default footer (bytes).'''
 
 	## Header Data
 	world_file_object.seek(40, 0)
-	header_size_numerical = struct.unpack('<i', world_file_object.read(4))[0]
-	world_size_numerical = struct.unpack('<i', world_file_object.read(4))[0]
-	gamemode_type = world_file_object.read(4)
-	footer_location = struct.unpack('<i', world_file_object.read(4))[0]
+	header_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
+	border_size_numerical = (header_size_numerical - 464)
+	world_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
+	gamemode_loader = world_file_object.read(4)
+	footer_location = struct.unpack('<I', world_file_object.read(4))[0]
 
 		# -Gamemode Handler
-	if gamemode_type == b'\x12\x00\x00\x00' or gamemode_type == b'\x00\x00\x00\x00': # -Creative / Flat || Auto
+	if gamemode_loader == b'\x12\x00\x00\x00' or gamemode_loader == b'\x00\x00\x00\x00': # -Creative / Flat || Auto
 		pass
 		# -Discover Footer Segment Null
 		discover_footer_size_numerical = None
 		discover_footer_location = None
-		chest_location = struct.unpack('<i', world_file_object.read(4))[0]
-		chest_size_numerical = struct.unpack('<i', world_file_object.read(4))[0]
-		entity_location = struct.unpack('<i', world_file_object.read(4))[0]
-		entity_size_numerical = struct.unpack('<i', world_file_object.read(4))[0]
 
-	elif gamemode_type == b'\x03\x00\x00\x01': # -Adventure / Survival
+		## Footer Normal Data
+			# -Chest Location/Size
+		world_file_object.seek(20, 1)
+		chest_location = struct.unpack('<I', world_file_object.read(4))[0]
+		chest_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
+
+			# -Entity Location/Size
+		world_file_object.seek(136, 1)
+		entity_location = struct.unpack('<I', world_file_object.read(4))[0]
+		entity_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
+
+	elif gamemode_loader == b'\x03\x00\x00\x01': # -Adventure / Survival
 		pass
 		# -Discover Footer Segment Set
-		discover_footer_size_numerical = struct.unpack('<i', world_file_object.read(4))[0]
+		discover_footer_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
 		discover_footer_location = footer_location
 		world_file_object.seek(4, 1)
-		footer_location = struct.unpack('<i', world_file_object.read(4))[0]
+		footer_location = struct.unpack('<I', world_file_object.read(4))[0]
 
-	#print(world_file_object.read(4)) # -Read 4 bytes - check position
+		## Footer Normal Data
+			# -Chest Location/Size
+		world_file_object.seek(20, 1)
+		chest_location = struct.unpack('<I', world_file_object.read(4))[0]
+		chest_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
 
+			# -Entity Location/Size
+		world_file_object.seek(136, 1)
+		entity_location = struct.unpack('<I', world_file_object.read(4))[0]
+		entity_size_numerical = struct.unpack('<I', world_file_object.read(4))[0]
 
 	## Properties Data
+	world_file_object.seek(256, 0)
+	world_properties_data = world_file_object.read(header_size_numerical - 256)
+	world_properties = world_components.World_Properties.Create_From_ByteString(world_properties_data)
 
 	## Decompressed Data
+	world_file_object.seek(header_size_numerical, 0)
+	compressed_world = world_file_object.read(world_size_numerical)
+	decompressed_world = Decompress_Byte_Segment(compressed_world)
 
 	## Footer Data
 
@@ -90,53 +126,37 @@ def Decompress_World_File(world_file_object: BinaryIO, **kwargs) -> Tuple[dict, 
 		# -Debug Text
 	if 'debug_text' in kwargs and kwargs['debug_text'] == True:
 		print(f"Header Size: {header_size_numerical} bytes.")
+		print(f"Border Size: {border_size_numerical} bytes.")
 		print(f"World Size: {world_size_numerical} bytes.")
+
 		if discover_footer_location:
 			print(f"Discover Footer Location: {discover_footer_location}.")
 			print(f"Discover Footer Size: {discover_footer_size_numerical} bytes")
+
 		print(f"Footer Location: {footer_location}.")
-		print(f"Footer Data:\n\tChest Location: {chest_location}\n\tChest Size: {chest_size_numerical}\n\tEntity Location:{entity_location}\n\tEntity Size:{entity_size_numerical}")
 
+		print("Footer Data:"
+			f"\n\tChest Location: {chest_location}"
+			f"\n\tChest Size: {chest_size_numerical}"
+			f"\n\tEntity Location: {entity_location}"
+			f"\n\tEntity Size: {entity_size_numerical}")
 
-	#return world_size_numerical
-
-def Decompress_World_Properties(*args, **kwargs) -> dict:
-	'''
-	Returns a dictionary of the world properties.'''
-
-	"""
-	World Name
-	World Size
-	Skybox Offset
-	Planet
-	Spawn Position
-	Player Position
-	Gamemode
-	Chest Data
-	Entity Data
-	Weather Data
-	Time Data
-	"""
-	pass
+	return decompressed_world, world_properties
 
 ##-------- Store World Data --------##
 
 def Export_World(world_properties: dict, decompressed_world: bytes, folder_path: str) -> None:
 	'''
 	Returns none, but writes the compressed file format to the folder path given.'''
-	export_world_data = Compress_World(world_properties, decompressed_world)
-
-	with open(folder_path, 'wb') as export_world:
-		export_world.write(export_world_data)
-
-def Compress_World(world_properties: dict, decompressed_world: bytes) -> bytes:
-	'''
-	Returns a bytestring of the compressed world.'''
 	pass
 
 ## Main
 
 ## Temporary
-data = Import_World("debug/[B-Data] Wooden Chest-O.dat")
+data = Import_World("debug/Debug_Map[0].dat")
+#data = Import_World("debug/Debug_Map[1].dat")
+#data = Import_World("debug/Debug_Adv[0].dat")
+#data = Import_World("debug/[Entity] Bear Brown-O.dat")
+#data = Import_World("debug/[B-Data] Wooden Chest-O.dat")
 
-print(data)
+#print(data)
